2da0bad2b44602124edac269eb6e4e7aa0b87896
RGUV16 <v.hewapathirana@rgu.ac.uk>
2024-10-20 12:30:08 +0530

    create

---
+++ cw_OOP/src/main/java/com/mycompany/cw_oop/Cw_OOP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cw_oop;
+
+/**
+ *
+ * @author HP
+ */
+public class Cw_OOP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
